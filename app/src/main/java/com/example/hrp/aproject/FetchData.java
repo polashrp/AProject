61d97675cd56f274591032db8dda4360e582ff24
polashrp <polashrp2640@gmail.com>
2017-11-25 01:38:47 +0600

    Complete json api task

---
+++ app/src/main/java/com/example/hrp/aproject/FetchData.java
@@ -0,0 +1,48 @@
+package com.example.hrp.aproject;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class FetchData extends AsyncTask <Void,Void,Void> {
+
+        String data = "";
+        @Override
+        protected Void doInBackground(Void... voids){
+
+            try {
+                URL url = new URL("https://api.myjson.com/bins/r1gcf");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream =  httpURLConnection.getInputStream();
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line = "";
+
+                while (line != null){
+                    line = bufferedReader.readLine();
+                    data = data + line;
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        MainActivity.data.setText(this.data);
+    }
+
+}
